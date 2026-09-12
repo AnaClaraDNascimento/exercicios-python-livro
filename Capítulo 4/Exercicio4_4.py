0c28c23e34efe011290703bd3fe5d310b7799587
AnaClaraDNascimento <anaclaradnascimento2010@gmail.com>
2026-09-11 23:05:25 -0300

    Update Exercicio4_4.py

--- Capítulo 4/Exercicio4_4.py
+++ Capítulo 4/Exercicio4_4.py
@@ -1,5 +1,4 @@
-#Exercício 4.4 Escreva um programa que pergunte o salário do funcionário e calcule o valor do aumento. 
-# Para salários superiores a R$ 1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, de 15%.
+#Exercício 4.4
 
 salario = float(input("Digite o valor do seu salário:"))
 
